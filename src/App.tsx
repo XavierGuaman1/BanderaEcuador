123f3a346a5da1f729edc3d82311164c93f58775
XavierGuaman1 <116098951+XavierGuaman1@users.noreply.github.com>
2024-01-19 08:46:31 -0500

    BanderaEcuador

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
+  const handleColorClick = (e: React.MouseEvent<HTMLDivElement>, color: string) => {
+    e.stopPropagation();
+    alert('El color es: ${color} - Ecuador');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="color-container">
+        <div className="color yellow" onClick={(e) => handleColorClick(e, 'amarillo')}>
+          <div className="color blue" onClick={(e) => handleColorClick(e, 'azul')}>
+            <div className="color red" onClick={(e) => handleColorClick(e, 'rojo')}>
+              Dale Click Para Saber El Color
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
